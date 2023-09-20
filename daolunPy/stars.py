43f3cd1c4eace8192d3af4b10cba94b96baf67c5
774596 <2238674546@qq.com>
2023-09-20 13:05:49 +0800

    2

--- daolunPy/stars.py
+++ daolunPy/stars.py
@@ -1,9 +1,9 @@
 #print("   ", end="")
-for i in range(18):
-    print(chr(0x2065), end="")
+for i in range(13):
+    print(chr(0x2605), end="")
 print(" ")
-print(chr(0x2065), "数据科学与工程导论", chr(0x2065))
+print(chr(0x2605), "数据科学与工程导论", chr(0x2605))
 #print("   ", end="")
-for i in range(18):
-    print(chr(0x2065), end="")
+for i in range(13):
+    print(chr(0x2605), end="")
 print(" ", end="")
